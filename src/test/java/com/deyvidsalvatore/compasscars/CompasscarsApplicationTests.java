494041d82e38d28846be2fd257aab017adfff2f3
Deyvid Santos <deyvidsantosdatascience@gmail.com>
2023-07-11 17:16:52 -0300

    Initial commit

---
+++ src/test/java/com/deyvidsalvatore/compasscars/CompasscarsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deyvidsalvatore.compasscars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompasscarsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
